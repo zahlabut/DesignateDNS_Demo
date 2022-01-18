039540fe315a23de50a1654b975c36970d9d9635
Arkady Shtempler <arkadysh@gmail.com>
2022-01-18 12:40:04 +0200

    Add files via upload
    Designate Demo

---
+++ Common.py
@@ -0,0 +1,94 @@
+import subprocess, sys, time, re
+
+
+import random
+
+def rand():
+    return str(random.randint(1,1000))
+
+def exec_command(command):
+    try:
+        print_in_color('\n--> ' + command+ '\n', 'blue')
+        result = subprocess.check_output(command, stdin=True, stderr=subprocess.STDOUT, shell=True,encoding='UTF-8')
+        print_in_color(result, 'green')
+
+        cont=choose_option_from_list(['yes','no'], 'To continue?')[1]
+        if cont=='yes':
+            pass
+        else:
+            sys.exit(1)
+        return {'ReturnCode': 0, 'CommandOutput': result}
+    except subprocess.CalledProcessError as e:
+        print_in_color(e, 'red')
+        cont=choose_option_from_list(['yes','no'], 'To continue?')[1]
+        if cont=='yes':
+            pass
+        else:
+            sys.exit(1)
+        return {'ReturnCode': e.returncode, 'CommandOutput': e.output}
+
+def print_in_color(string,color_or_format=None):
+    string=str(string)
+    class bcolors:
+        HEADER = '\033[95m'
+        OKBLUE = '\033[94m'
+        OKGREEN = '\033[92m'
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
+    if color_or_format == 'green':
+        print(bcolors.OKGREEN + string + bcolors.ENDC)
+    elif color_or_format =='red':
+        print(bcolors.FAIL + string + bcolors.ENDC)
+    elif color_or_format =='yellow':
+        print(bcolors.WARNING + string + bcolors.ENDC)
+    elif color_or_format =='blue':
+        print(bcolors.OKBLUE + string + bcolors.ENDC)
+    elif color_or_format =='bold':
+        print(bcolors.BOLD + string + bcolors.ENDC)
+    else:
+        print(string)
+
+def choose_option_from_list(list_object, msg):
+    print('')
+    try:
+        if (len(list_object)==0):
+            print("Nothing to choose :( ")
+            print("Execution will stop!")
+            time.sleep(5)
+            exit("Connot continue execution!!!")
+            sys.exit(1)
+        print(msg)
+        counter=1
+        for item in list_object:
+            print(str(counter)+') - '+item)
+            counter=counter+1
+        choosed_option=input("Choose your option:")
+        if choosed_option=='Demo':
+            return [True, 'Demo']
+        while (int(choosed_option)<0 or int(choosed_option)> len(list_object)):
+            print("No such option - ", choosed_option)
+            choosed_option=input("Choose your option:")
+        print_in_color("Option is: '"+list_object[int(choosed_option)-1]+"'"+'\n','bold')
+        return [True,list_object[int(choosed_option)-1]]
+    except Exception as e:
+        print('*** No such option!!!***', e)
+        return[False, str(e)]
+
+
+def get_ips_from_file(fil='/etc/bind/named.conf.options'):
+    with open(fil) as fh:
+        fstring = fh.readlines()
+    ip_list=[]
+    for line in fstring:
+        ipPattern = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+        findIP = re.findall(ipPattern, line)
+        if len(findIP)>0:
+            for ip in findIP:
+                if ip not in ip_list:
+                    ip_list.append(ip)
+    return ip_list
+
+
---
+++ Demo.py
@@ -0,0 +1,88 @@
+# Usage
+# You need to be sourced (admintrc, openrc ...) to run this script
+
+from Common import *
+
+random_string = rand()
+
+sec_gr_commands = [
+"openstack security group list --project=admin",
+"openstack security group rule create $(openstack security group list --project=admin | grep default | awk '{print $2}') --protocol icmp --ingress",
+"openstack security group rule create $(openstack security group list --project=admin | grep default | awk '{print $2}') --protocol icmp --egress",
+"openstack security group rule create $(openstack security group list --project=admin | grep default | awk '{print $2}') --protocol tcp --dst-port 22 --ingress",
+"openstack security group rule create $(openstack security group list --project=admin | grep default | awk '{print $2}') --protocol tcp --dst-port 22 --egress",
+"openstack security group rule create $(openstack security group list --project=admin | grep default | awk '{print $2}') --protocol tcp --dst-port 80 --ingress",
+"openstack security group rule create $(openstack security group list --project=admin | grep default | awk '{print $2}') --protocol tcp --dst-port 80 --egress"]
+
+net_commands=[
+    'openstack network create net{}'.format(random_string),
+    'openstack subnet create subnet{} --network net{} --subnet-range 192.0.2.0/24'.format(random_string,random_string)]
+
+zone_to_net_commands=[
+    'openstack zone create example{}.com. --email example1@example.com'.format(random_string),
+    'openstack zone list --all',
+    'openstack network list',
+    'openstack network set --dns-domain example{}.com. net{}'.format(random_string, random_string),
+    'openstack network show net{}'.format(random_string)
+]
+
+create_router_commands = [
+    'openstack router create router{}'.format(random_string),
+    'openstack router set --external-gateway public router{}'.format(random_string),
+    'openstack router add subnet router{} subnet{}'.format(random_string,random_string),
+    'openstack router show router{}'.format(random_string)
+]
+
+create_vm_commands=[
+    'openstack flavor list',
+    'openstack image list',
+    'openstack server create --flavor m1.micro --image cirros-0.5.1-x86_64-disk --nic net-id=net{} vm{}'.format(random_string,random_string),
+    'openstack server list'
+]
+
+create_fip_for_vm_port_commands=[
+    'openstack port list --server vm{}'.format(random_string),
+    "openstack floating ip create public --port $(openstack port list --server vm{}".format(random_string)+" | grep ip_address | awk '{print $2}')"
+]
+
+
+# for com in sec_gr_commands:
+#     exec_command(com)
+
+for com in net_commands:
+    exec_command(com)
+
+for com in zone_to_net_commands:
+    exec_command(com)
+
+for com in create_router_commands:
+    exec_command(com)
+
+for com in create_vm_commands:
+    exec_command(com)
+
+for com in create_fip_for_vm_port_commands:
+    exec_command(com)
+
+
+
+### Configure local resolver with a NEW created zone ###
+exec_command('sudo mkdir -p /etc/systemd/resolved.conf.d')
+local_resolver_ip = get_ips_from_file()[0]
+zone_data_file='example{}.conf'.format(random_string)
+zone_data='[Resolve]\n'
+zone_data+='DNS={}\n'.format(local_resolver_ip)
+zone_data+='Domains=~example{}.com'.format(random_string)
+fil = open(zone_data_file,'w')
+fil.write(zone_data)
+fil.close()
+exec_command('sudo mv '+zone_data_file+' '+'/etc/systemd/resolved.conf.d/'+zone_data_file)
+exec_command('sudo systemctl restart systemd-resolved')
+
+designate_demo_commands=[
+    'openstack recordset list example{}.com.'.format(random_string),
+    'dig @{} vm{}.example{}.com. A'.format(local_resolver_ip, random_string,random_string)
+]
+
+for com in designate_demo_commands:
+    exec_command(com)

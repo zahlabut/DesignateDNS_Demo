97fa32b689424894c96a5f543fbeea2e90cc653b
Arkady Shtempler <arkadysh@gmail.com>
2022-11-24 21:15:25 +0200

    Add scale test scenario

--- ScaleTest.py
+++ ScaleTest.py
@@ -1,21 +1,23 @@
 from dataclasses import dataclass
 from Common import *
+import ipaddress, threading
 
 print_in_color('You must be sourced with *.rc file, for example overcloudrc', 'green')
+IPs = [str(ip) for ip in ipaddress.IPv4Network('192.0.0.0/12')]
 
 @dataclass
 class TestScale:
-    """Class for keeping track of an item in inventory."""
-    source_file: str = 'overcloudrc'
-    number_of_zones: int = 1000
-    number_of_recordsets: int = 100
+    number_of_zones: int
+    number_of_recordsets: int
+    start_index: int
 
     @staticmethod
-    def exec_command(command):
+    def exec_command(command, exit_on_error=False):
         try:
             print_in_color('\n' + command + '\n', 'blue')
-            result = subprocess.check_output(command, stdin=True, stderr=subprocess.STDOUT, shell=True,
-                                             encoding='UTF-8')
+            result = subprocess.check_output(
+                command, stdin=True, stderr=subprocess.STDOUT, shell=True,
+                encoding='UTF-8')
             clear_result = ''
             for line in result.splitlines():
                 if line.startswith('/') or line.startswith(' '):
@@ -23,29 +25,56 @@ class TestScale:
                 else:
                     clear_result += line + '\n'
             print_in_color(clear_result, 'green')
-            cont = to_continue()
-            if cont == 'y':
-                pass
-            else:
-                sys.exit(1)
             return (0, result)
         except subprocess.CalledProcessError as e:
-            print_in_color(e, 'red')
-            cont = to_continue()
-            if cont == 'y':
-                pass
+            if exit_on_error:
+                print_in_color('Failed to execute: "{}"'.format(command), 'red')
+                print_in_color(e, 'red')
+                sys(exit(1))
             else:
-                sys.exit(1)
-            return (e.returncode, e.output)
-
+                print_in_color(e, 'red')
+                return (e.returncode, e.output)
 
     def create_zone(self, zone_name, zone_email):
         command = 'openstack zone create --email {} {}'.format(zone_email, zone_name)
-        return_code, returned_output = self.exec_command(command)
-        if return_code != 0:
-            print_in_color('Failed to execute: "{}"'.format(command), 'red')
-            print_in_color(returned_output, 'red')
-            sys(exit(1))
+        self.exec_command(command, exit_on_error=True)
+
+    def create_A_recordset(self, zone_name, record_name, record_ip):
+        command = 'openstack recordset create {} --type A {} --record {}'.format(zone_name, record_name, record_ip)
+        self.exec_command(command, exit_on_error=True)
+
+    def create_zones_with_recordsets(self):
+        for zone in range(self.start_index, self.start_index+self.number_of_zones):
+            zone_name, zone_email = 'example{}.com.'.format(zone), 'example{}@example{}.com'.format(zone, zone)
+            self.create_zone(zone_name, zone_email)
+            for record in range(self.start_index, self.start_index+self.number_of_recordsets):
+                record_name = 'vm{}'.format(record)
+                self.create_A_recordset(zone_name, record_name, IPs[record])
 
 
+if __name__ == "__main__":
+    # Increase quota
+    zones = 10*1000*1000
+    zone_recordsets = 100
+    command = 'openstack dns quota set --zone-recordsets {} --zones {}'.format(zone_recordsets, zones)
+    TestScale.exec_command(command)
 
+    # # Start stuff creation in SERIAL
+    # obj = TestScale(number_of_zones=10, number_of_recordsets=3, start_index=40)
+    # obj.create_zones_with_recordsets()
+
+    # Start threads
+    threads_number = 10
+    zones=100
+    recordsets=10
+    start_index=0
+
+    start_indexes = [x for x in range(0, threads_number * threads_number, threads_number)]
+    threads=[]
+    for index in start_indexes:
+        threads.append(TestScale(number_of_zones=zones, number_of_recordsets=recordsets, start_index=index))
+    for obj in threads:
+        print(obj.number_of_zones, obj.number_of_recordsets, obj.start_index)
+    for t in threads:
+        thread = threading.Thread(target=t.create_zones_with_recordsets)
+        thread.start()
